7238b1f0e74eeba2522d4d29f0fdd48e02875a30
Nuruddin <nuruddin.iminohunov@gmail.com>
2017-05-08 21:47:04 -0500

    Share left

--- InstagramClone/CameraViewController.swift
+++ InstagramClone/CameraViewController.swift
@@ -7,82 +7,126 @@
 //
 
 import UIKit
-import FirebaseStorage
-import FirebaseDatabase
+import AVFoundation
 
 class CameraViewController: UIViewController {
     @IBOutlet weak var photo: UIImageView!
-
     @IBOutlet weak var captionTextView: UITextView!
-    
+    @IBOutlet weak var removeButton: UIBarButtonItem!
     @IBOutlet weak var shareButton: UIButton!
-    var selectedImage : UIImage?
-    
-    
+    var selectedImage: UIImage?
+    var videoUrl: URL?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleSelectPhoto))
         photo.addGestureRecognizer(tapGesture)
         photo.isUserInteractionEnabled = true
-    
+        
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        handlePost()
+    }
     
-    func handleSelectPhoto(){
+    func handlePost() {
+        if selectedImage != nil {
+            self.shareButton.isEnabled = true
+            self.removeButton.isEnabled = true
+            self.shareButton.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        } else {
+            self.shareButton.isEnabled = false
+            self.removeButton.isEnabled = false
+            self.shareButton.backgroundColor = .lightGray
+            
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func handleSelectPhoto() {
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
+        pickerController.mediaTypes = ["public.image", "public.movie"]
         present(pickerController, animated: true, completion: nil)
-
-        
     }
-    
-   
-    @IBAction func shareButton_touchUpInside(_ sender: UIButton) {
-        ProgressHUD.show("Waiting....", interaction: false)
-        if let shareImg = self.selectedImage, let imageData = UIImageJPEGRepresentation(shareImg, 0.6){
-            let photoID = NSUUID().uuidString
-            let storageRef = FIRStorage.storage().reference(forURL:Config.STORAGE_ROOT_REF).child("posts").child(photoID)
-            storageRef.put(imageData, metadata: nil, completion:{
-                (metadata, error) in
-                if error != nil{
-                    return
-                }
-                let photoURL = metadata?.downloadURL()?.absoluteString
-                self.sendDataToDatabase(photoURL: photoURL!)
-                
+    @IBAction func shareButton_TouchUpInside(_ sender: Any) {
+        view.endEditing(true)
+        ProgressHUD.show("Waiting...", interaction: false)
+        if let profileImg = self.selectedImage, let imageData = UIImageJPEGRepresentation(profileImg, 0.1) {
+            let ratio = profileImg.size.width / profileImg.size.height
+            HelperService.uploadDataToServer(data: imageData, videoUrl: self.videoUrl, ratio: ratio, caption: captionTextView.text!, onSuccess: {
+                self.clean()
+                self.tabBarController?.selectedIndex = 0
             })
             
-        } else{
-            ProgressHUD.showError("Image is empty")
+        } else {
+            ProgressHUD.showError("Profile Image can't be empty")
         }
-
     }
-
+    @IBAction func remove_TouchUpInside(_ sender: Any) {
+        clean()
+        handlePost()
+    }
     
-    func sendDataToDatabase(photoURL: String){
-        let ref = FIRDatabase.database().reference()
-        let postsReference = ref.child("posts")
-        let newPostID = postsReference.childByAutoId().key
-        let newPostReference = postsReference.child(newPostID)
-        newPostReference.setValue(["photoURL":photoURL], withCompletionBlock: {
-        (error, ref) in
-            if error != nil {
-                ProgressHUD.showError(error!.localizedDescription)
-                return
-            }
-            ProgressHUD.showSuccess("Success")
-            })
-        
+    func clean() {
+        self.captionTextView.text = ""
+        self.photo.image = UIImage(named: "placeholder-photo")
+        self.selectedImage = nil
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "filter_segue" {
+            let filterVC = segue.destination as! FilterViewController
+            filterVC.selectedImage = self.selectedImage
+            filterVC.delegate = self
+        }
+    }
+    
 }
-
-
-extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("did Finish Picking Media")
+        print(info)
+        
+        if let videoUrl = info["UIImagePickerControllerMediaURL"] as? URL {
+            if let thumnailImage = self.thumbnailImageForFileUrl(videoUrl) {
+                selectedImage = thumnailImage
+                photo.image = thumnailImage
+                self.videoUrl = videoUrl
+            }
+            dismiss(animated: true, completion: nil)
+        }
+        
         if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
             selectedImage = image
             photo.image = image
+            dismiss(animated: true, completion: {
+                self.performSegue(withIdentifier: "filter_segue", sender: nil)
+            })
         }
-        dismiss(animated: true, completion: nil)
+    }
+    
+    func thumbnailImageForFileUrl(_ fileUrl: URL) -> UIImage? {
+        let asset = AVAsset(url: fileUrl)
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        do {
+            let thumbnailCGImage = try imageGenerator.copyCGImage(at: CMTimeMake(7, 1), actualTime: nil)
+            return UIImage(cgImage: thumbnailCGImage)
+        } catch let err {
+            print(err)
+        }
+        
+        return nil
+    }
+}
+
+extension CameraViewController: FilterViewControllerDelegate {
+    func updatePhoto(image: UIImage) {
+        self.photo.image = image
+        self.selectedImage = image
     }
 }
